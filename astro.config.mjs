9d2f6b340c7d3edb10d74a3a216a2a0a68598b4b
Ignacio Alvarez <ignacioalvarezcal>
2025-06-29 22:34:39 -0400

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import AutoImport from "astro-auto-import";
 import icon from "astro-icon"; // https://www.astroicon.dev/guides/upgrade/v1/
-import vercel from "@astrojs/adapter-vercel";
+import vercel from "@astro/adapter-vercel";
 
 // https://astro.build/config
 export default defineConfig({
